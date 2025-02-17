7eb5cc1ced0c3d940acd57482a6a72e832c15fc9
obikiel <ezekielobiomachi1@gmail.com>
2025-02-17 08:06:08 +0100

    database added

--- Tasklist/settings.py
+++ Tasklist/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'Tasklist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'postgres-service',  # Name of the Kubernetes Service
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
